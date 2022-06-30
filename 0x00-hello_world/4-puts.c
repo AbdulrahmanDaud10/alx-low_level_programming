e4c9541d3e137066bb3fdb1075f8c3056b6ab3ad
AbdulrahmanDaud10 <dulisyke3015@gmail.com>
2022-06-30 10:01:21 -0700

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry point
  *
- * return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle\n");
+		puts("\"Programming is like building a multilingual puzzle");
 		return (0);
 }
