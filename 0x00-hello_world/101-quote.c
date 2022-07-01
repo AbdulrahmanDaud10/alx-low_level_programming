7b8aa51e69bb769f22f495deaa867d87b43cb6c5
AbdulrahmanDaud10 <dulisyke3015@gmail.com>
2022-07-01 07:23:33 -0700

    second trial

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,8 @@
  */
 int main(void)
 {
-	char quote='\"and that piece of art is useful" - Dora Korpar, 2015-10-19\n';
-	putchar(quote);
+	write(2,
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-1\n"
+		,59);
 	return (1);
 }
