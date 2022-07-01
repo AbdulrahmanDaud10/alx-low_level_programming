f6949de23a9ecdf5808331b74cc7c6ee4e92f643
AbdulrahmanDaud10 <dulisyke3015@gmail.com>
2022-07-01 07:10:20 -0700

    trial

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	char quote='\"and that piece of art is useful" - Dora Korpar, 2015-10-19\n';
+	putchar(quote);
+	return (1);
+}
