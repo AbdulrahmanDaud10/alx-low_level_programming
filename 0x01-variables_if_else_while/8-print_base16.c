cfb3451e9833b5bff28a1c741ba5b0d5dda0c6e5
AbdulrahmanDaud10 <dulisyke3015@gmail.com>
2022-07-02 05:43:51 -0700

    fix

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,7 +10,7 @@ int main(void)
 	int numbers;
 	char alphabets;
 
-	for (numbers = 0; numbers <= 10; numbers++)
+	for (numbers = 0; numbers < 10; numbers++)
 		putchar((numbers % 10) + '0');
 
 	for (alphabets = 'a'; alphabets <= 'f'; alphabets++)
