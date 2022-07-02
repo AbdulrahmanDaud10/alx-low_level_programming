fd69671ff288e8cfbbdcf6020769d46393adcde2
AbdulrahmanDaud10 <dulisyke3015@gmail.com>
2022-07-02 04:29:35 -0700

    betty hardcore stuff

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,10 +10,10 @@ int main(void)
 	char alphabet;
 
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-		{
-			if (alphabet != 'e' && alphabet != 'q')
-				putchar(alphabet);
-		}
+	{
+		if (alphabet != 'e' && alphabet != 'q')
+			putchar(alphabet);
+	}
 
 		putchar('\n');
 
