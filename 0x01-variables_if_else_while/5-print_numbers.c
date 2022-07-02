ca2cb7275cc09871af667f2d8b41d908bbf0c472
AbdulrahmanDaud10 <dulisyke3015@gmail.com>
2022-07-02 04:55:14 -0700

    Numbers betty fix

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,11 +9,10 @@ int main(void)
 {
 	int number;
 
-	for (number = 0; number <= 10; number++)
+	for (number = 0; number < 10; number++)
 		printf("%d", number);
 
 	printf("\n");
 
 	return (0);
 }
-	
