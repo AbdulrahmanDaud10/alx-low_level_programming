0ebc668bba4abf40a0c4892f9697c76d5ebcb82b
AbdulrahmanDaud10 <dulisyke3015@gmail.com>
2022-07-02 05:05:29 -0700

    Numberz fix

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+		putchar((number % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
