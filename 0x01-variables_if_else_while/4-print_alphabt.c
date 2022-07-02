521249dba9d8ec7e44f25fbb1b3f6b17e74acfdf
AbdulrahmanDaud10 <dulisyke3015@gmail.com>
2022-07-02 04:25:57 -0700

    Betty style fix

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,14 +9,13 @@ int main(void)
 {
 	char alphabet;
 
-	for(alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		{
 			if (alphabet != 'e' && alphabet != 'q')
 				putchar(alphabet);
 		}
 
 		putchar('\n');
-
 
 		return (0);
 }		
