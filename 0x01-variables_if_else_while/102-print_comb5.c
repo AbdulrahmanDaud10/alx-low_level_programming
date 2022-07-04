ed51424ef993e24fa3a671b530cdf71148631b8f
AbdulrahmanDaud10 <dulisyke3015@gmail.com>
2022-07-04 07:33:18 -0700

    last task

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int m, n;
+
+	for (m = 0; m <= 98; m++)
+	{
+		for (n = m + 1; n <= 99; n++)
+		{
+			putchar((m / 10) + '0');
+			putchar((m % 10) + '0');
+			putchar(' ');
+			putchar((n / 10) + '0');
+			putchar((n % 10) + '0');
+
+			if (m == 98 && n == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
