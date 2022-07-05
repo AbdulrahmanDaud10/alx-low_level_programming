51782d696524b3f2ef1862b91934bf036623b8aa
AbdulrahmanDaud10 <dulisyke3015@gmail.com>
2022-07-05 06:14:42 -0700

    alphabet guessing game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the letters of the alphabet in lowercase
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+
+	_putchar('\n');
+}
