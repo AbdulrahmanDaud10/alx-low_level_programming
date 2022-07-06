01260fe7bf67fe15cbb0cf8df76272490022b4fd
AbdulrahmanDaud10 <dulisyke3015@gmail.com>
2022-07-06 02:45:40 -0700

    fix

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,26 +1,26 @@
 #include "main.h"
-#include "stdio.h"
 
 /**
  * print_sign - checks sign of a number
- * @n: letter tested
+ *@n: letter tested
  * return: 0 (Success)
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+");
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		printf("0");
+		_putchar('0');
 		return (0);
 	}
-	else
+	else if (n < 0)
 	{
-		printf("-");
+		_putchar('-');
 		return(-1);
 	}
+	return (0);
 }
