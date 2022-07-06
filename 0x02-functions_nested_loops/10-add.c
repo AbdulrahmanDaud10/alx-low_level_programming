82afabd60731f56e0f84460b5b6c2cf28cc2d571
AbdulrahmanDaud10 <dulisyke3015@gmail.com>
2022-07-06 05:53:59 -0700

    Betty fix

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,9 +2,9 @@
 
 /**
  * add - adds two integer and returns the result
- *@x - value to add
- *@y - value to add
- *@add - addition
+ *@x: value to add
+ *@y: value to add
+ *@add: addition
  * Return: result
  */
 int add(int x, int y)
