972cfef7ed962ed986aa0bed6c3c3a048035da0a
AbdulrahmanDaud10 <dulisyke3015@gmail.com>
2022-07-06 07:18:36 -0700

    final task solve

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - prints numerical value from n to 98 followed by new line
+ *@n: to check
+ *Return: 0 (Success)
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for ( n = n; n <= 97; n++)
+			printf("%d, ", n);
+
+	}
+	else
+		for (n = n; n > 98; n--)
+			printf("%d, ", n);
+	printf("98\n");
+}
