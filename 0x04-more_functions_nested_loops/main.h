d114ada6ecbd15460f24c8669d9a755bed02f16f
AbdulrahmanDaud10 <dulisyke3015@gmail.com>
2022-07-08 11:20:09 -0700

    main header file fix

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_FILE
+#define MAIN_FILE
+
 void print_alphabet(void);                                                                                              
 void _putchar(char x);                                                                                                  
 void print_alphabet(void);                                                                                              
@@ -11,4 +14,6 @@ void jack_bauer(void);
 void times_table(void);                                                                                                 
 int add(int, int);                                                                                                      
 void print_to_98(int n);                                                                                                
-void print_times_table(int n); 
+void print_times_table(int n);
+
+#endif
