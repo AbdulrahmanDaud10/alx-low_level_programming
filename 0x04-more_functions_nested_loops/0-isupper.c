0f3c6ebd8b5d2f94dbc9f2178b03064137329f71
AbdulrahmanDaud10 <dulisyke3015@gmail.com>
2022-07-08 10:32:48 -0700

    solve task onw

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,10 +2,10 @@
 
 /**
  * _isupper - checks for uppercase character
- *@c: the character to be checked
+ * @c: the character to be checked
  * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
-		return (c >= 'A' && c <= 'Z');
+	return (c >= 'A' && c <= 'Z');
 }
