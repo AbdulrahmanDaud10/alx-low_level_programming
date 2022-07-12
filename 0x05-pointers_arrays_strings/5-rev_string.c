26cfb99ba9ea1a42f288e49e56eca73d3ba01f9b
AbdulrahmanDaud10 <dulisyke3015@gmail.com>
2022-07-12 05:05:30 -0700

    Task 6th solve

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: paramter tested
+ *
+ * Return: void.
+ */
+void rev_string(char *s)
+{
+	char myCharacter;
+	int i = strlen(s);
+	int j = 0;
+
+	while (j < i)
+	{
+		i--;
+		myCharacter = s[j];
+		s[j] = s [i];
+		s[i] = myCharacter;
+		j += 1;
+	}
+}
