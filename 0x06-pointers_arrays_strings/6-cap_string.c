59727b2f0850f26642fa740d52c8c1d8e3e0cf8e
AbdulrahmanDaud10 <dulisyke3015@gmail.com>
2022-07-14 03:53:00 -0700

    string to upper cases solve

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,21 +4,36 @@
  * cap_string - Capitalizes all letters of string
  * @c: each character of a string to be converted to uppercase
  *
- * Description: according to ascii uppercase leeter are 65 to 90
- * to convert letter of a string to uppercase the formalae is
- * character += 32
  * Return: c (Success)
  */
-char *cap_string(char *c)
+char *cap_string(char *string)
 {
-	int i;
+	int i, c;
+	int trigger;
+	char nots[] = ",;.!?(){}\n\t\"";
 
-	for (i = 0; c[i] != '\0'; i++)
+	for (i = 0, trigger = 0; string[i] != '\0'; i++)
 	{
-		if (c[i] > 65 && c[i] <= 90)
+		if (string[0] > 96 && string[0] < 123)
+			trigger = 1;
+		for (c = 0; nots[c] != '\0'; c++)
 		{
-			c[i] += 32;
+			if (nots[c] == string[i])
+				trigger = 1;
+		}
+
+		if (trigger)
+		{
+			if (string[i] > 96 && string[i] < 123)
+			{
+				string[i] -= 32;
+				trigger = 0;
+			}
+			else if (string[i] > 64 && string[i] < 91)
+				trigger = 0;
+			else if (string[i] > 47 && string[i] < 58)
+				trigger = 0;
 		}
 	}
-	return (c);
+	return (string);
 }
