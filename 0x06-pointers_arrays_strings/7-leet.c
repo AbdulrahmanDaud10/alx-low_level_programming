6b8bfd6466ce56cc474a3d5c0278a50984b39a98
AbdulrahmanDaud10 <dulisyke3015@gmail.com>
2022-07-14 04:35:55 -0700

    encoding string solution

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - encode given string to 1337
+ * @c: string
+ *
+ * Return: encoded string c
+ */
+char *leet(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		while (c[i] == 'a' || c[i] == 'A')
+			c[i] = '4';
+		while (c[i] == 'e' || c[i] == 'E')
+			c[i] = '3';
+		while (c[i] == 'o' || c[i] == 'O')
+			c[i] = '0';
+		while (c[i] == 't' || c[i] == 'T')
+			c[i] = '7';
+		while (c[i] == 'l' || c[i] == 'L')
+			c[i] = '1';
+	}
+	return (c);
+}
