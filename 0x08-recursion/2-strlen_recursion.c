0a6778fc5aa1d0665c576e9c87f64858fa249084
AbdulrahmanDaud10 <dulisyke3015@gmail.com>
2022-07-20 08:03:15 -0700

    different code write up to fix betty style issue

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,16 +7,7 @@
  */
 int _strlen_recursion(char *s)
 {
-	int length = 0;
-
 	if (*s == '\0')
-	{
-		return 0;
-	}
-	else
-	{
-		return length = 1 + _strlen_recursion(s + 1);
-	}
-
-	return (length);
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
 }
