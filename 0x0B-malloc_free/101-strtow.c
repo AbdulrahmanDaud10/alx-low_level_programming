0dd47aee699657c62435d72068cc6bac93ce4601
AbdulrahmanDaud10 <dulisyke3015@gmail.com>
2022-07-26 12:18:04 -0700

    corrections to errors

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -10,6 +10,7 @@
 int count_word(char *s)
 {
 	int flag, c, w;
+
 	flag = 0;
 	w = 0;
 
@@ -38,6 +39,7 @@ char **strtow(char *str)
 {
 	char **matrix, *tmp;
 	int i, k = 0, len = 0, words, c = 0, start, end;
+
 	while (*(str + len))
 	len++;
 	words = count_word(str);
