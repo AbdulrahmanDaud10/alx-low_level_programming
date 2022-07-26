5e6d6cf9760e1d778d78aa34ea94e0b9b75c69fe
AbdulrahmanDaud10 <dulisyke3015@gmail.com>
2022-07-26 12:49:17 -0700

    task three solve

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,57 +1,39 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
-* * _strlen - finds length of a given string.
-* * @s: pointer to string of char type
-* *
-* * Return: length of given string.
-*/
-
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-		i++;
-	return (i);
-}
-
-
-/**
-* * str_concat - Concatenates 2 strings.
-* * @s1: First string.
-* * @s2: Second string.
-* *
-* * Return: Returns the created array.
+* str_concat - concatenate two strings using malloc
+* @s1: string 1
+* @s2: string 2
+* Return: pointer to concat string
 */
 
 char *str_concat(char *s1, char *s2)
 {
-	int i, j, len1, len2, tlen;
-	char *ar;
+	char *a;
+	int i, j, c, d;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
-	tlen = (len1 + len2) + 1;
-	ar = malloc(tlen * sizeof(char));
-	if (ar == NULL)
+
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+
+	a = malloc((i * sizeof(*s1)) + (j * sizeof(*s2)) + 1);
+	if (a == NULL)
 		return (NULL);
-	else
+
+	for (c = 0, d = 0; c < (i + j + 1); c++)
 	{
-		for (i = 0; i < len1; i++)
-			ar[i] = s1[i];
-		for (j = 0; j < len2; j++)
-		{
-			ar[i] = s2[j];
-			i++;
-		}
-		ar[i + 1] = '\0';
-		return (ar);
+		if (c < i)
+			a[c] = s1[c];
+		else
+			a[c] = s2[d++];
 	}
+
+	return (a);
 }
