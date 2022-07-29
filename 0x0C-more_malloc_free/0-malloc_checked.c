80455f728fd8fada573c66b7bfbaf5b1dc66217f
Abdulrahman Daud Miraj <60228187+AbdulrahmanDaud10@users.noreply.github.com>
2022-07-29 19:42:32 +0300

    Update 0-malloc_checked.c
    fix

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,23 +2,23 @@
 #include "main.h"
 
 /**
- *  * malloc_checked - allocates memory using malloc
- *	* @x: amount of memory to allocate (in bytes)
- *	* Return: pointer to allocated memory
- *	*/
+ * malloc_checked - allocates memory using malloc
+ * @x: amount of memory to allocate (in bytes)
+ * Return: pointer to allocated memory
+ */
 void *malloc_checked(unsigned int x)
 {
-		/*declare a pointer to be used*/
-		int *ptr;
+	/*declare a pointer to be used*/
+	int *ptr;
 
-			/*reserve memory spaces in heap*/
-			ptr = malloc(x);
+	/*reserve memory spaces in heap*/
+	ptr = malloc(x);
 
-				/*if malloc doesn't fails*/
-				if (ptr == NULL)
-						{
-									free(ptr);
-											exit(98); /*Terminate process with 98 as return value*/
-												}
-					return (ptr);
+	/*if malloc doesn't fails*/
+	if (ptr == NULL)
+	{
+		free(ptr);
+		exit(98); /*Terminate process with 98 as return value*/
+	}
+	return (ptr);
 }
