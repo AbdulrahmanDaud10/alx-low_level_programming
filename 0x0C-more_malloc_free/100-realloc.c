1dd47072d389df3338e62a368ed66342ceb9b40f
AbdulrahmanDaud10 <dulisyke3015@gmail.com>
2022-07-29 09:25:32 -0700

    solving Betty style problem

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -32,7 +32,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size > old_size)
 	{
 		for (i = 0; i < old_size; i++)
-		new_ptr[i] = old_ptr[i]:
+		new_ptr[i] = old_ptr[i];
 	}
 													free(ptr);											return (new_ptr);
 }
