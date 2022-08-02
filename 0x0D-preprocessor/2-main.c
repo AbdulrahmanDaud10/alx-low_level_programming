7afa60e53d82a9a94e49c8f823a36cce5e78bf4b
AbdulrahmanDaud10 <dulisyke3015@gmail.com>
2022-08-01 23:05:10 -0700

    preprocessor project solutions

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file
+ *
+ * Description: prints name of the file
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+	printf("%s\n", _FILE_);
+	return (0);
+}
