3a4d22492d48a64c53a5640121a100ac04ee4687
Abdulrahman Daud Miraj <60228187+AbdulrahmanDaud10@users.noreply.github.com>
2022-08-03 22:37:43 +0300

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,41 +1,44 @@
-#include "3-calc.h"
-#include <stdlib.h>
+include "3-calc.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * main - main function
- * @argc: argument count
- * @argv: string of arguments in array
- * Return: 0
- */
+ * main - function that Prints result of an operation
+ * @argc: Number of arguments
+ * @argv: Value of arguments
+ * Return: Returns an int.
+**/
 
 int main(int argc, char *argv[])
 {
-	int a, b;
-	int (*o)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (argv[2][1] != '\0')
+
+	if (
+		(strcmp(argv[2], "+") &&
+		strcmp(argv[2], "-") &&
+		strcmp(argv[2], "*") &&
+		strcmp(argv[2], "/") &&
+		strcmp(argv[2], "%"))
+	)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	o = get_op_func(argv[2]);
-	if (o == NULL)
+	if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0)
+		&& atoi(argv[3]) == 0)
 	{
 		printf("Error\n");
-		exit(99);
+		exit(100);
 	}
 
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-
-	printf("%d\n", o(a, b));
+	printf("%d\n", (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3])));
 
 	return (0);
 }
