b245ff05c241a1db03673c637c3ca22ea7b9d085
Abdulrahman Daud Miraj <60228187+AbdulrahmanDaud10@users.noreply.github.com>
2022-08-11 19:14:17 +0300

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,35 +1,45 @@
 #include "lists.h"
-#include <string.h>
-#include <stdio.h>
 
 /**
- * add_node - add a new node at the beginning of `list_t` list
- * @head: double pointer to head
- * @str: string to be saved in new node, must be duplicated
- * Return: Address of new element or NULL if failed
+ * add_node - add new node at the beginning
+ * @head: head of the list
+ * @str: string data of the list to be added
+ * Return: the address of the new element, or NULL if it failed
  */
+
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node;
-	int c;
+	list_t *new;
 
-	for (c = 0; dup_str[c] != '\0'; c++)
-		;
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
 	{
-		free(new_node);
 		return (NULL);
 	}
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
+	new->str = strdup(str);
+	if (new->str == NULL)
 	{
-		free(new_node);
+		free(new);
 		return (NULL);
 	}
-	new_node->len = c;
-	new_node->next = *head;
-
-	*head = new_node;
+	new->len = _strlen(str);
+	new->next = *head;
+	*head = new;
 	return (*head);
+}
+
+/**
+ * _strlen - function to calculate the length of a const char string
+ * @s: string to work with
+ * Return: length of string.
+ */
+
+int _strlen(const char *s)
+{
+	unsigned int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
 }
