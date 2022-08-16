974ed3fbe4666cada76448c4d60ecb54fb115722
Abdulrahman Daud Miraj <60228187+AbdulrahmanDaud10@users.noreply.github.com>
2022-08-16 17:37:55 +0300

    Update 100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,39 +1,26 @@
 #include "lists.h"
-#include <stdio.h>
+
 /**
- * reverse_listint - Reverse a `listint_t` linked list
- * @head: double pointer to head
- * Description: Limited to going through loop only once.
- * Not allowed to use `malloc` or `free` or arrays
- * Can only declare a max of 2 variables.
- * Return: pointer to first node of reversed list
+ * reverse_listint - Reverses a list.
+ * @head: pointer to the address of list_t list's head.
+ * Return: pointer to the first node of the reversed list.
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *hold;
-	listint_t *current;
+listint_t *after, *before;
+if (head == NULL || *head == NULL)
+{
+return (NULL);
+}
 
-	if (*head == NULL)
-		return (NULL);
-
-	current = *head;
-	*head = current->next;
-	hold = (*head)->next;
-	current->next = NULL;
-	if (*head == NULL)
-	{
-		*head = current;
-		return (current);
-	}
-
-	while (hold != NULL)
-	{
-		(*head)->next = current;
-		current = *head;
-		*head = hold;
-		hold = (*head)->next;
-	}
-
-	(*head)->next = current;
-	return (*head);
+before = NULL;
+while ((*head)->next != NULL)
+{
+after = (*head)->next;
+(*head)->next = before;
+before = *head;
+*head = after;
+}
+(*head)->next = before;
+return (*head);
 }
