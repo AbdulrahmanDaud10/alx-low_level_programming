57adaf635a7f875cc24ba3cbdc6bdce65482d469
AbdulrahmanDaud10 <dulisyke3015@gmail.com>
2022-08-16 04:24:33 -0700

    finction returning elements ina linked list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -7,11 +7,9 @@
  */
 size_t listint_len(const listint_t *h)
 {
-	const listint_t *c;
-	size_t count;
+	size_t b;
 
-	for (c = h, count = 0; c != NULL; count++, c = c->next)
-		;
-
-	return (count);
+	for (b = 0; h; b++)
+		h = h->next;
+	return (b);
 }
