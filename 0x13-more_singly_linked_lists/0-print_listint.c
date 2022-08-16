216cab001f0bdfa2e88b4ddd69646a0217393d6f
AbdulrahmanDaud10 <dulisyke3015@gmail.com>
2022-08-16 03:43:57 -0700

    function that prints nodes in a linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "list.h"
+
+/**
+ * print_listint - prints all elements of a list_t list
+ * @h: singly list printed
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t i;
+
+	for (i = 0; h; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (i);
+}
