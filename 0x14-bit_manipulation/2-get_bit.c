796c9b58ca801321a083875493924bfafa4c6166
AbdulrahmanDaud10 <dulisyke3015@gmail.com>
2022-08-18 01:25:22 -0700

    function that returns value of a bit at a given time

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * get_bit - function that returns the value of a bit at a given index
+ * @n: input long integer
+ * @index: index to start at
+ * Return: value of the bit at hte index
+ */
+int gget_bit(unsigned long int n, unsigned int index)
+{
+	return ((index > (8 * sizeof(n) - 1)) ? -1 : (int)(n >> index) & 1);
+}
